297a45f50952d85e7d8eddaa94c292c1ee8c6830
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-04-06 14:21:01 +0300

    feat: move Auth interface to auth folder

--- src/application/Auth.ts
+++
@@ -1,9 +0,0 @@
-import { EmailAddress } from "../domain/auth/EmailAddress";
-import { Password } from "../domain/auth/Password";
-import { AccessToken } from "../domain/auth/AccessToken";
-
-export interface Auth {
-  login(email: EmailAddress, password: Password): Promise<AccessToken>;
-  logout(): Promise<void>;
-  checkToken(token: AccessToken): Promise<boolean>;
-}
