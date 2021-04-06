e4cab6ed0d9afa71f50a41831a561825ef0e6686
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-04-06 14:18:23 +0300

    feat: add Auth service interface

---
+++ src/application/Auth.ts
@@ -0,0 +1,9 @@
+import { EmailAddress } from "../domain/auth/EmailAddress";
+import { Password } from "../domain/auth/Password";
+import { AccessToken } from "../domain/auth/AccessToken";
+
+export interface Auth {
+  login(email: EmailAddress, password: Password): Promise<AccessToken>;
+  logout(): Promise<void>;
+  checkToken(token: AccessToken): Promise<boolean>;
+}
