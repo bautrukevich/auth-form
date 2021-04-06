d39cf90464bd9355b8f29d005a1b142e4a2f3106
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-04-06 12:55:23 +0300

    feat: add router to app

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
+
 import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
