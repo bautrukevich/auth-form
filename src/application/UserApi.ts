f4bfe69182577bd14d54ad6dc4aabfc151967d0d
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-04-06 13:49:54 +0300

    feat: add UserApi interface

---
+++ src/application/UserApi.ts
@@ -0,0 +1,7 @@
+import { User } from "../domain/user/User";
+import { FullName } from "../domain/user/FullName";
+
+export interface UserApi {
+  getUserInfo(): Promise<User>;
+  updateUserInfo(fullName: FullName): Promise<void>;
+}
