801bffb46374d4c3b19aafa776b1731428874be9
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-04-09 02:30:23 +0300

    chore: add context to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { FormEvent } from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import React, { FormEvent, useEffect } from "react";
+import { Route, Switch, Redirect, useHistory } from "react-router-dom";
 
 import { Page } from "./ui/page/Page";
 import { Header } from "./ui/header/Header";
@@ -14,16 +14,32 @@ import { Button } from "./ui/form/button/Button";
 import { Footer } from "./ui/footer/Footer";
 
 import { useFormWithValidation } from "./infrastructure/hooks/useFormWithValidation";
+import { useAuthState } from "./infrastructure/contexts/AuthStateContext";
 
 function App() {
+  const history = useHistory();
+  const { isLoggedIn, accessToken, signInWithEmailAndPassword, hasError, restoreToken, signOut } = useAuthState();
   const { handleChange, values, isValid, errors } = useFormWithValidation();
   const isDisabled = !isValid;
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log(values, isValid, errors);
+    await signInWithEmailAndPassword(values["email"], values["password"]);
   };
 
-  const isLoggedIn = false;
+  console.log(isLoggedIn, accessToken, hasError);
+
+  useEffect(() => {
+    (async () => {
+      await restoreToken();
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      history.push("/");
+    }
+  }, [history, isLoggedIn]);
 
   return (
     <Page>
@@ -31,7 +47,15 @@ function App() {
       <Content>
         <Switch>
           <Route exact path="/">
-            {() => (isLoggedIn ? <Wrapper title="Добро пожаловать" /> : <Redirect to="/accounts/login" />)}
+            {() =>
+              isLoggedIn ? (
+                <Wrapper title="Добро пожаловать">
+                  <Button onClick={signOut}>Выйти</Button>
+                </Wrapper>
+              ) : (
+                <Redirect to="/accounts/login" />
+              )
+            }
           </Route>
           <Route exact path="/accounts/login">
             <Wrapper title="Войти">
