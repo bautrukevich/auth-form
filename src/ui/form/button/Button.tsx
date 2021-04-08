5a5dc41f54348e1c38c8383a0ef723715300ac6f
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-04-09 02:29:55 +0300

    fix: Button (add onClick and set type "button" by default)

--- src/ui/form/button/Button.tsx
+++ src/ui/form/button/Button.tsx
@@ -4,13 +4,19 @@ import styles from "./button.module.css";
 import clsx from "clsx";
 
 type SubmitButtonProps = React.PropsWithChildren<{
-  type: "button" | "submit" | "reset";
+  type?: "button" | "submit" | "reset";
   disabled?: boolean;
+  onClick?: () => void;
 }>;
 
-export const Button = ({ type, disabled = false, children }: SubmitButtonProps) => {
+export const Button = ({ type = "button", disabled = false, onClick, children }: SubmitButtonProps) => {
   return (
-    <button className={clsx(styles.button, disabled && styles.button_inactive)} type={type} disabled={disabled}>
+    <button
+      onClick={onClick}
+      className={clsx(styles.button, disabled && styles.button_inactive)}
+      type={type}
+      disabled={disabled}
+    >
       {children}
     </button>
   );
