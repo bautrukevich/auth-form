37df51f386f0b0f5e2783e599a91666bc75c4749
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-04-09 02:29:11 +0300

    feat: add sign in with email and password test

---
+++ src/application/__tests__/auth/useCase/signInWithEmailAndPassword.test.ts
@@ -0,0 +1,24 @@
+import * as SignInWithEmailAndPassword from "../../../auth/useCase/signInWithEmailAndPassword";
+import { SECURE_AUTH_STATE_KEY } from "../../../../infrastructure/contexts/AuthStateContext";
+import { SECRET_EMAIL, SECRET_PASSWORD, SECRET_TOKEN } from "../../../../infrastructure/__tests__/data";
+import { FakeAuth } from "../../../../infrastructure/FakeAuth";
+import { CookiesSecureStorage } from "../../../../infrastructure/CookiesSecureStorage";
+import { AuthStateKey } from "../../../../domain/auth/AuthStateKey";
+import { AccessToken } from "../../../../domain/auth/AccessToken";
+
+describe("signInWithEmailAndPassword", () => {
+  it("should be able to sign in with email and password", async () => {
+    const auth = new FakeAuth();
+    const storage = new CookiesSecureStorage<AuthStateKey, AccessToken>(AccessToken.fromString);
+
+    const query = SignInWithEmailAndPassword.Query.create({
+      email: SECRET_EMAIL,
+      password: SECRET_PASSWORD,
+      authStateKey: SECURE_AUTH_STATE_KEY,
+    });
+    const handler = new SignInWithEmailAndPassword.Handler({ auth, storage });
+    const accessToken = AccessToken.fromString(SECRET_TOKEN);
+
+    await expect(handler.handle(query)).resolves.toStrictEqual(accessToken);
+  });
+});
