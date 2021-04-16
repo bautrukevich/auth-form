c767a3fa4dcb8c44834117d589527e6722ffc3c1
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-04-16 10:51:56 +0300

    chore: remove query from use case

--- src/application/user/useCase/getUserInfo.ts
+++ src/application/user/useCase/getUserInfo.ts
@@ -1,22 +1,5 @@
 import { UserApi } from "../UserApi";
-import { AccessToken } from "../../../domain/auth/AccessToken";
 import { User } from "../../../domain/user/User";
-
-export class Query {
-  private readonly _accessToken: string;
-
-  private constructor({ accessToken }: { accessToken: string }) {
-    this._accessToken = accessToken;
-  }
-
-  static create({ accessToken }: { accessToken: string }): Query {
-    return new Query({ accessToken });
-  }
-
-  get accessToken(): AccessToken {
-    return AccessToken.fromString(this._accessToken);
-  }
-}
 
 export class Handler {
   private readonly userApi: UserApi;
