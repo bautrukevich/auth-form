29b3831c0e484badad64c523ed479f80fd89881f
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-04-19 10:45:21 +0300

    fix: use form hook

--- src/infrastructure/hooks/useFormWithValidation.ts
+++ src/infrastructure/hooks/useFormWithValidation.ts
@@ -1,24 +1,20 @@
 import { ChangeEventHandler, useState } from "react";
 
-// export const useFormWithValidation = () => {
-//   const [values, setValues] = useState<Record<string, string>>({});
-//   const [errors, setErrors] = useState<Record<string, string>>({});
-//   const [isValid, setIsValid] = useState<boolean>(false);
-//
-//   const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
-//     const target = event.target;
-//     const name = target.name;
-//     const value = target.value;
-//     const form = target.closest("form");
-//
-//     setValues((values) => {
-//       return { ...values, [name]: value };
-//     });
-//     setErrors((errors) => {
-//       return { ...errors, [name]: target.validationMessage };
-//     });
-//     setIsValid(() => form !== null && form.checkValidity());
-//   };
-//
-//   return { values, handleChange, errors, isValid };
-// };
+export const useFormWithValidation = () => {
+  const [values, setValues] = useState<Record<string, string>>({});
+  const [errors, setErrors] = useState<Record<string, string>>({});
+  const [isDisabled, setIsDisabled] = useState<boolean>(true);
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    const form = target.closest("form");
+
+    setValues((values) => ({ ...values, [name]: value }));
+    setErrors((errors) => ({ ...errors, [name]: target.validationMessage }));
+    setIsDisabled(form !== null && !form.checkValidity());
+  };
+
+  return { values, handleChange, errors, isDisabled };
+};
